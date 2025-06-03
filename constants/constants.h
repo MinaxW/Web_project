af27a50aabfc13dddf5d77e54439e446cbbff132
Прахова Анастасия <nastya2006prahova@gmail.com>
2025-06-03 18:21:35 +0300

    Добавлена обновлённая папка constants

--- constants/constants.h
+++ constants/constants.h
@@ -2,16 +2,21 @@
 #define CONSTANTS_H
 
 // Базовые пути
-#define DIR_TEMPLATES "./templates/"
-#define DIR_CSS       "./css/"
+#define DIR_TEMPLATES "src/templates/"
+#define DIR_CSS       "src/css/"
 
 // Пути к HTML-шаблонам
 #define PATH_LOGIN_HTML DIR_TEMPLATES   "login.html"
 #define PATH_SUCCESS_HTML DIR_TEMPLATES "main_screen.html"
 #define PATH_ERROR_HTML DIR_TEMPLATES   "error.html"
+#define PATH_RESULT_HTML DIR_TEMPLATES "result.html"
+#define PATH_NOT_VALID_HTML DIR_TEMPLATES "not_valid.html"
 
 // Путь к CSS
-#define PATH_CSS_STYLES DIR_CSS "styles.css"
+#define PATH_LOGIN_CSS DIR_CSS "login.css"
+#define PATH_ERROR_CSS DIR_CSS "error.css"
+#define PATH_MAIN_SCREEN_CSS DIR_CSS "main_screen.css"
+#define PATH_RESULT_CSS DIR_CSS "result.css"
 
 //Content-Type заголовки
 #define CONTENT_TYPE_HTML "Content-Type: text/html; charset=utf-8\r\n"
